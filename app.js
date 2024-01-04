034dc975c5a15dd03c7a84f90515a9cbb8d9d235
Ziru Zou <jiyucoding@yahoo.com>
2024-01-04 01:18:40 -0500

    setup middleware and routes

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express")
 const app = express()
+const lists = require('./routes/lists')
 
 const connectDB = require("./db/connect")
 require('dotenv').config()
+
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())     // - express.json() middleware, in order to use the 'req.body'
+
+
+// routes
+// app.get('/', (req, res) => {res.send("Task Manage Appz")})
+app.use('/api/v1/to_do_list', lists)
+
+
+
 
 const port = process.env.PORT || 3000;
 
@@ -12,9 +26,7 @@ const start = async () => {
         await connectDB(process.env.MONGO_URI);
         app.listen(port, () => console.log(`Server is listening on port ${port}...`));
     } catch (error) {
-        console.log("errrorororororoor")
         console.log(error);
     }
-  };
-  
+};
 start();
