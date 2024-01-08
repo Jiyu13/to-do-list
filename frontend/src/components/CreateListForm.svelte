5c24c8dba4e409a2403f859b13797b0fbf166d33
Ziru Zou <jiyucoding@yahoo.com>
2024-01-08 03:43:41 -0500

    style createListForm

--- frontend/src/components/CreateListForm.svelte
+++ frontend/src/components/CreateListForm.svelte
@@ -1,12 +1,88 @@
-<form class="list-form">
-    <div>
-        <input
-            class="list-name-input"
-            type="text"
-            name="'list-name"
-            bind:value={input}
-            placeholder="List name"
-        />
-        <button>Add</button>
+<script>
+    import CloseSrc from '../../public/icons/close_24.svg';
+
+
+    let newListNameInput
+    export let handleAddLists
+
+</script>
+
+<div class="modal-container">
+    <div class="modal-dialog">
+        <div class="pop-up-header">
+            <div class="header-title">Create a List</div>
+
+            <button class="close-btn" on:click={handleAddLists}>
+                <img src={CloseSrc} alt="close button"/>
+            </button>
+        </div>
+        
+        <form class="list-form">
+            <input
+                class="list-name-input"
+                type="text"
+                name="'list-name"
+                bind:value={newListNameInput}
+                placeholder="List name"
+            />
+            <button class="add-list-btn">Add</button>
+        </form>
     </div>
-</form>
+
+</div>
+
+
+<style>
+    .list-form {
+        display: flex;
+        gap: 12px;
+        justify-content: center;
+        margin-top: 1.8rem;
+    }
+    .list-name-input {
+        padding: 8px;
+    }
+
+    .add-list-btn {
+        background-color: rgba(100, 125, 135, 0.8);
+        border-radius: 4px;
+        padding: 0 8px;
+        color: white;
+    }
+    button.add-list-btn:hover {
+        background-color: rgb(100, 125, 135);
+    }
+
+    .pop-up-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .header-title {
+        flex-grow: 1;
+        text-align: center;
+        font-size: 1.3rem;
+        font-weight: bold;
+        padding: 24px 16px 8px 16px;
+        color: rgb(100, 125, 135);
+
+    }
+    .close-btn {
+        background: none;
+        border: none;
+        padding: 0;
+        font: inherit;
+        cursor: pointer;
+        outline: inherit;
+        margin: 0;
+        padding: 4px;
+
+        position: relative;
+        top: -12px;
+    }
+    button.close-btn:hover {
+        background-color: rgb(251, 246, 238);
+        border-radius: 0 4px 0 0 ;
+    }
+
+</style>
