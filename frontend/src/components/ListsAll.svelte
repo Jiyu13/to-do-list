16898b4a38a546d0d43b1ebf47341e692b7af312
Ziru Zou <jiyucoding@yahoo.com>
2024-01-08 11:27:41 -0500

    render allLists

---
+++ frontend/src/components/ListsAll.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let allLists
+    export let handleDelete
+
+    import SingleList from "./SingleList.svelte"
+
+</script>
+
+<div class="lists-container">
+    {#each allLists as list}
+        <SingleList 
+            list={list} 
+            handleDelete={handleDelete}
+        />
+    {/each}
+</div>
+
+<style>
+    .lists-container {
+		display: flex;
+		gap: 12px;
+		flex-direction: column;
+		overflow-y: auto; 
+		max-height: 70vh;
+	}
+</style>
