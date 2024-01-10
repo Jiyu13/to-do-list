8613fa737e9c6e2ee047749986c72ac2295ec612
Ziru Zou <jiyucoding@yahoo.com>
2024-01-09 20:32:26 -0500

    add if statement to check if passed in list is empty

--- frontend/src/components/ListsAll.svelte
+++ frontend/src/components/ListsAll.svelte
@@ -7,20 +7,12 @@
 </script>
 
 <div class="lists-container">
-    {#each allLists as list}
-        <SingleList 
-            list={list} 
-            handleDelete={handleDelete}
-        />
-    {/each}
+    {#if allLists}
+        {#each allLists as list}
+            <SingleList 
+                list={list} 
+                handleDelete={handleDelete}
+            />
+        {/each}
+    {/if}
 </div>
-
-<style>
-    .lists-container {
-		display: flex;
-		gap: 12px;
-		flex-direction: column;
-		overflow-y: auto; 
-		max-height: 70vh;
-	}
-</style>
