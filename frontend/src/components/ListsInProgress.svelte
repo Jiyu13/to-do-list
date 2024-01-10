8144b419a4185a8f81b44f3e2d959d43adda0391
Ziru Zou <jiyucoding@yahoo.com>
2024-01-09 20:31:30 -0500

    create function

---
+++ frontend/src/components/ListsInProgress.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let inProgressLists
+    export let handleDelete
+
+    import SingleList from "./SingleList.svelte"
+
+</script>
+
+<div class="lists-container">
+        {#if inProgressLists}
+            {#each inProgressLists as list}
+                <SingleList 
+                    list={list} 
+                    handleDelete={handleDelete}
+                />
+            {/each}
+        {/if}
+    
+</div>
+
