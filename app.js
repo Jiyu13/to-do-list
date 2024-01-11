e773f880e1af4306b0af58fec69f688775a26010
Ziru Zou <jiyucoding@yahoo.com>
2024-01-10 20:43:06 -0500

    add static middleware & path to frontend/public, does not workgit add app.jsgit add app.js!

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ require('dotenv').config()
 // middleware
 app.use(cors());
 app.use(express.json())     // - express.json() middleware, in order to use the 'req.body'
-
+app.use(express.static('./frontend/public'))
 
 // routes
 // app.get('/', (req, res) => {res.send("Task Manage Appz")})
 app.use('/api/v1/to_do_list', listsRoutes)
-
+const path = require("path")
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html')); // Adjust the path as needed
+});
 
 
 
