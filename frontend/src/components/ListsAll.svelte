4ac384fdfb4572e2a7b4905416a6acb4ebaea333
Ziru Zou <jiyucoding@yahoo.com>
2024-01-16 12:27:07 -0500

    handle filtering by searchTerm; create reactive statement for filteredLists

--- frontend/src/components/ListsAll.svelte
+++ frontend/src/components/ListsAll.svelte
@@ -3,11 +3,14 @@
     import axios from "axios";
 	import { onMount } from "svelte"
     import SingleList from "./SingleList.svelte"
-    import { allLists, API_URL } from "../store.js"
+    import { allLists, API_URL, searchTerm } from "../store.js"
     
+    let filteredLists
+
     onMount(async() => {
 		const {data} = await axios.get(API_URL + "api/v1/to_do_list")
 		allLists.set(data["lists"])
+        filteredLists = data["lists"]
 	})
 
     function handleDeleteFromAll(id) {
@@ -15,12 +18,15 @@
 		allLists.update(currentLists => currentLists.filter(l => l._id !== id))
 	}
 
-
+    $: filteredLists = $allLists.filter(l => {
+        const listTitle = l.name.toLowerCase();
+        return $searchTerm === "" || listTitle.includes($searchTerm.toLowerCase());
+    });
 </script>
 
 <div class="lists-container">
-    {#if $allLists}
-        {#each $allLists as list}
+    {#if filteredLists}
+        {#each filteredLists as list}
             <SingleList 
                 list={list} 
                 handleDelete={handleDeleteFromAll}
