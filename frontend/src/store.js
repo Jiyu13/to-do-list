6e45e9eec23637429bbad08d0147b02e65a12127
Ziru Zou <jiyucoding@yahoo.com>
2024-01-16 12:17:48 -0500

    define searchTerm as svelte store & export

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,9 +6,10 @@ let doneLists = writable([])
 let API_URL="http://localhost:3000/"
 let pathname = writable(window.location.pathname)
 let isEdit = writable(false)
+let searchTerm = writable("")
 
 export {
     allLists, inProgressLists, doneLists, 
     API_URL, pathname,
-    isEdit
+    isEdit, searchTerm
 }
