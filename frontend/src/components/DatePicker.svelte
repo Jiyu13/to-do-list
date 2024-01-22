51e1695e1a13a3844d938d178f290fd08d04b3c4
Ziru Zou <jiyucoding@yahoo.com>
2024-01-22 15:48:45 -0500

    remove component

--- frontend/src/components/DatePicker.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    const locale = {
-        en: {
-            days: "Su|Mo|Tu|We|Th|Fr|Sa".split("|"),
-            months: "Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec".split('|'),
-            start: 0,
-        }
-  }
-</script>
