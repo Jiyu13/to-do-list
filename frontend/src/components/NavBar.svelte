9d555bdaea4119959c387df14ac6d5e32ec08baf
Ziru Zou <jiyucoding@yahoo.com>
2024-01-23 02:19:22 -0500

    rename component & render MenuMobile if isMoble=true

---
+++ frontend/src/components/NavBar.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { searchTerm } from "../store.js";
+    import MenuMobile from "./MenuMobile.svelte";
+
+    import OrderBy from "./OrderBy.svelte";
+
+
+    export let isMobile
+    export let handleOpenAddList
+    
+
+
+    function handleSearchChange(value) {
+		searchTerm.set(value)
+    }
+    
+</script>
+    <div class="left-navbar">
+        {#if isMobile}
+            <MenuMobile handleOpenAddList={handleOpenAddList}/>
+        {/if}
+
+        <OrderBy />
+        
+        <input
+            class="search-bar"
+            type="text"
+            placeholder="search"
+            bind:value={$searchTerm}
+            on:input={(e) => handleSearchChange(e.target.value)}
+        />
+    </div>
+
+
+<style></style>
