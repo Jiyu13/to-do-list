d03cd17bd4f4540e149f508a44544aef65054f6d
Ziru Zou <jiyucoding@yahoo.com>
2024-01-23 02:23:08 -0500

    create component for order by button

---
+++ frontend/src/components/OrderBy.svelte
@@ -0,0 +1,24 @@
+<script>
+    import listSrc from '../../public/icons/list_24.svg';
+    let isOpen = false
+</script>
+
+<div position="relative">
+    <div class="trigger-container">
+        <div class="trigger-wrapper">
+            <img src={listSrc} alt="order by menu"/>
+            <div>Order by</div>
+        </div>
+
+        {#if isOpen }
+            <ul class="options-box">
+                <li id="">Date, new to old</li>
+                <li id="">Date, old to new</li>
+                <li id="">Name, A - Z</li>
+                <li id="">Name, Z - A</li>
+
+            </ul>
+        {/if}
+
+    </div>
+ </div>
