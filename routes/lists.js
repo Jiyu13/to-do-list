b4899ff1f5673d03b898416f5b90dd78de82152f
Ziru Zou <jiyucoding@yahoo.com>
2024-01-25 20:26:01 -0500

    add route for handling 'PUT' request to update list by id

--- routes/lists.js
+++ routes/lists.js
@@ -6,7 +6,7 @@ const {
     getAllLists,  getInProgressLists, getCompletedLists,
     getList, 
     createList,
-    updateList,
+    updateList, updateWholeList,
     deleteList,
 } = require('../controllers/lists')
 
@@ -14,5 +14,6 @@ router.route('/').get(getAllLists).post(createList)
 router.route('/in-progress').get(getInProgressLists)
 router.route('/completed').get(getCompletedLists)
 router.route('/:id').get(getList).patch(updateList).delete(deleteList)
+router.route('/:id').put(updateWholeList)
 
 module.exports = router
